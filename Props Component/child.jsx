87d772e1f14a96266bf4bc0fa1be835a745c02b5
Avni mujawdiya <avnimujawdiya008@gmail.com>
2026-02-12 01:38:45 +0530

    Added complete React learning project

---
+++ Props Component/child.jsx
@@ -0,0 +1,8 @@
+// Child component
+
+function student(props){
+    return(
+        <h2>Name: {props.name}</h2>
+    );
+}
+export default Student;
---
+++ Props Component/parent.jsx
@@ -0,0 +1,8 @@
+// / Parent component
+function App()
+{
+    return(
+        <student name="Avni"/>
+    );
+}
+export default App;
